ef2fa8753d2b33f3027794d8d0e05ef6c8897ce6
fazzar1 <fares.azzar@yahoo.com>
2020-01-29 16:19:50 +0100

    ajout de la classe Test

---
+++ ProgramTest.java
@@ -0,0 +1,15 @@
+package com.capgemini.demo;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProgramTest {
+
+    @Test
+    public void addNodeTest() {
+        assertEquals(1,1);
+    }
+
+}
